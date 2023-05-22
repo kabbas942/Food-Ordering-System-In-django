12ab77a83486ed38f5a3a454ed5da73dce6ffd9e
Muhammad Abbas <kabbas942@gmail.com>
2023-05-22 20:06:37 +0500

    third commit

--- foodOrdingSystem/foodOrdingSystem/settings.py
+++ foodOrdingSystem/foodOrdingSystem/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foodStuff.apps.FoodstuffConfig'
+    'foodStuff.apps.FoodstuffConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
